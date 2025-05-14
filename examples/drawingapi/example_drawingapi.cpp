77a278661ffb70d27316003545b89daff965e29e
Tinic Uro <tinicuro@gmail.com>
2025-05-13 21:28:20 -0700

    .

---
+++ examples/drawingapi/example_drawingapi.cpp
@@ -0,0 +1,77 @@
+#include "constixel.h"
+
+#include "fonts/pixeloperator_bold_mono.h"
+using myfont = constixel::pixeloperator_bold_mono;
+
+#include "fonts/ibmplexsans_bold_32_aa.h"
+using aafont = constixel::ibmplexsans_bold_32_aa;
+
+#include "fonts/ibmplexsans_bold_32_mono.h"
+using monofont = constixel::ibmplexsans_bold_32_mono;
+
+#include "fonts/ibmplexsans_bold_18_aa.h"
+using aafontsmall = constixel::ibmplexsans_bold_18_aa;
+
+#include "fonts/ibmplexsans_bold_18_mono.h"
+using monofontsmall = constixel::ibmplexsans_bold_18_mono;
+
+int main() {
+    using namespace constixel;
+
+    image<format_8bit, 1024, 384, 2> image;
+
+    int32_t x_pos = 20;
+    int32_t y_pos = 20;
+    image.fill_rect(x_pos,y_pos, 128, 64, color::RED);
+    image.draw_string_centered_mono<myfont>(x_pos+64,70+y_pos,"fill_rect",color::WHITE);
+
+    x_pos += 200;
+    image.fill_round_rect(x_pos, y_pos, 128, 64, 16, color::BLUE);
+    image.draw_string_centered_mono<myfont>(x_pos+64,70+y_pos,"fill_round_rect",color::WHITE);
+
+    x_pos += 200;
+    image.fill_round_rect_aa(x_pos, y_pos, 128, 64, 16, color::GREEN);
+    image.draw_string_centered_mono<myfont>(x_pos+64,70+y_pos,"fill_round_rect_aa",color::WHITE);
+
+    x_pos += 200;
+    image.fill_circle(x_pos+16, y_pos+32, 32, color::CYAN);
+    image.draw_string_centered_mono<myfont>(x_pos+16,70+y_pos,"fill_circle",color::WHITE);
+
+    x_pos += 200 - 64;
+    image.fill_circle_aa(x_pos+16, y_pos+32, 32, color::MAGENTA);
+    image.draw_string_centered_mono<myfont>(x_pos+16,70+y_pos,"fill_circle_aa",color::WHITE);
+
+    x_pos = 20;
+    y_pos += 100;
+    image.draw_line(x_pos, y_pos, x_pos+128, y_pos+64, color::GRAY_20, 3);
+    image.draw_string_centered_mono<myfont>(x_pos+64,70+y_pos,"draw_line",color::WHITE);
+
+    x_pos += 200;
+    image.draw_line_aa(x_pos, y_pos, x_pos+128, y_pos+64, color::GRAY_20);
+    image.draw_string_centered_mono<myfont>(x_pos+64,70+y_pos,"draw_line_aa",color::WHITE);
+
+    x_pos += 200;
+    image.stroke_rect(x_pos, y_pos, 128, 64, color::GRAY_20, 4);
+    image.draw_string_centered_mono<myfont>(x_pos+64,70+y_pos,"stroke_rect",color::WHITE);
+
+    x_pos = 20;
+    y_pos += 100;
+    image.draw_string_mono<monofont, true>(x_pos, y_pos + 20, "ABCabcAVA", color::GRAY_20);
+    image.draw_string_centered_mono<myfont>(x_pos+80,70+y_pos,"draw_string_mono",color::WHITE);
+
+    x_pos += 200;
+    image.draw_string_aa<aafont, true>(x_pos, y_pos + 20, "ABCabcAVA", color::GRAY_20);
+    image.draw_string_centered_mono<myfont>(x_pos+80,70+y_pos,"draw_string_aa",color::WHITE);
+
+    image.draw_string_mono<monofontsmall, true, DEGREE_90>(700, y_pos + 20, "ABCabcAVA", color::GRAY_20);
+    image.draw_string_mono<monofontsmall, true, DEGREE_270>(700, y_pos + 20, "ABCabcAVA", color::GRAY_20);
+    image.draw_string_mono<monofontsmall, true, DEGREE_180>(700, y_pos + 20, "ABCabcAVA", color::GRAY_20);
+    image.draw_string_centered_mono<myfont>(700,130+y_pos,"draw_string_mono",color::WHITE);
+
+    image.draw_string_aa<aafontsmall, true, DEGREE_90>(900, y_pos + 20, "ABCabcAVA", color::GRAY_20);
+    image.draw_string_aa<aafontsmall, true, DEGREE_270>(900, y_pos + 20, "ABCabcAVA", color::GRAY_20);
+    image.draw_string_aa<aafontsmall, true, DEGREE_180>(900, y_pos + 20, "ABCabcAVA", color::GRAY_20);
+    image.draw_string_centered_mono<myfont>(900,130+y_pos,"draw_string_aa",color::WHITE);
+    
+    image.sixel_to_cout();
+}
